1d594589f1516e173c85255b84d300a4ba2d64c0
BennoAlif <bennoalif41@gmail.com>
2020-01-07 18:04:24 +0700

    First init

---
+++ src/latihanmvc/event/PelangganListener.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanmvc.event;
+
+import latihanmvc.model.PelangganModel;
+
+/*
+    Nama        : Benno Alif Anggara
+    Kelas       : IF1
+    NIM         : 10118024
+*/
+public interface PelangganListener {
+    public void onChange(PelangganModel pelanggan);
+}
